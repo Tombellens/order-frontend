c7a8f058b0062cc3d7a357c98ed0d9bf7ec88713
Tombellens <tombellens96@hotmail.com>
2020-05-06 15:50:47 +0200

    updated server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-on-package.json>'));
+app.use(express.static(__dirname + '/dist/ng-order-fe'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile('index.html', {root: 'dist/<name-on-package.json>'});
+  res.sendFile('index.html', {root: 'dist/ng-order-fe'});
 });
 
 // Start the app by listening on the default Heroku port
